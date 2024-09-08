28172f61cd97b498e6dadfc793add15a3863eac8
Ritik Kandoria <39797888+kandoria@users.noreply.github.com>
2024-09-08 19:40:56 +0530

    Implemented Extensions.

---
+++ src/main/java/com/verve/verveChallenge/config/RedisConfig.java
@@ -0,0 +1,41 @@
+package com.verve.verveChallenge.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisHost);
+        redisStandaloneConfiguration.setPort(redisPort);
+
+        JedisClientConfiguration jedisClientConfiguration = JedisClientConfiguration.builder().build();
+
+        return new JedisConnectionFactory(redisStandaloneConfiguration, jedisClientConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+
+        return  template;
+    }
+}
--- src/main/java/com/verve/verveChallenge/service/UniqueRequestCounterService.java
+++ src/main/java/com/verve/verveChallenge/service/UniqueRequestCounterService.java
@@ -4,42 +4,54 @@ import com.verve.verveChallenge.controller.UniqueRequestCounterController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.*;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.Objects;
 import com.verve.verveChallenge.exception.HttpRequestException;
 
 @Service
 public class UniqueRequestCounterService {
     private static final Logger logger = LoggerFactory.getLogger(UniqueRequestCounterController.class);
     private static final Logger fileLogger = LoggerFactory.getLogger("FileLogger");
-    private final Map<String, Set<Integer>> minuteRequests = new ConcurrentHashMap<>();
+//    private final Map<String, Set<Integer>> minuteRequests = new ConcurrentHashMap<>();
 
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
     @Autowired
     private RestTemplate restTemplate;
 
     public void processRequests(int id, String endPoint) throws Exception {
         String currentMinute = getCurrentMinute();
-        minuteRequests.computeIfAbsent(currentMinute, k -> ConcurrentHashMap.newKeySet()).add(id);
+//        minuteRequests.computeIfAbsent(currentMinute, k -> ConcurrentHashMap.newKeySet()).add(id);
+        Long result = redisTemplate.opsForSet().add(currentMinute, String.valueOf(id));
+        boolean isUnique = Objects.equals(result, 1L);
 
-        if(endPoint!=null && !endPoint.isEmpty()) {
+        if(isUnique && endPoint!=null && !endPoint.isEmpty()) {
             int uniqueCount = getUniqueCount(currentMinute);
-            sendHttpGetRequest(endPoint, uniqueCount);
+            ResponseEntity<String> response = sendHttpPostRequest(endPoint, uniqueCount);
+            logger.info("HTTP POST request sent to {}. Status code: {}", endPoint, response.getStatusCode());
         }
     }
 
+    // Scheduled tot get unique counts in last minute.
     @Scheduled(fixedRate = 60000)
     public void logUniqueRequests() {
         String previousMinute = getPreviousMinute();
         int uniqueCount = getUniqueCount(previousMinute);
         fileLogger.info("Unique requests in the last minute: {}", uniqueCount);
-        minuteRequests.remove(previousMinute);
+        sentToKafka(uniqueCount);
+        redisTemplate.delete(previousMinute);
     }
 
     private String getCurrentMinute() {
@@ -51,10 +63,17 @@ public class UniqueRequestCounterService {
     }
 
     private int getUniqueCount(String minute) {
-        Set<Integer> requests = minuteRequests.get(minute);
-        return requests!=null ? requests.size() : 0;
+        Long size = redisTemplate.opsForSet().size(minute);
+        return size != null ? size.intValue() : 0;
     }
 
+    private void sentToKafka(int uniqueCount) {
+        String message = String.format("Unique requests in the last minute: %d", uniqueCount);
+        kafkaTemplate.send("unique-request-counts", message);
+        logger.info("Sent to Kafka: {}", message);
+    }
+
+    // Created as Part 1 implementation
     private void sendHttpGetRequest(String endPoint, int count) throws Exception {
         try {
             String url = String.format("%s?count=%d", endPoint, count);
@@ -65,6 +84,26 @@ public class UniqueRequestCounterService {
         }
         catch (Exception e) {
             throw new Exception("Failed to process http request: " + e.getMessage());
+        }
+    }
+
+    private ResponseEntity<String> sendHttpPostRequest(String endPoint, int count) throws Exception {
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            Map<String, Object> requestBody = new HashMap<>();
+            requestBody.put("uniqueCount", count);
+            requestBody.put("timestamp", LocalDateTime.now().toString());
+
+            HttpEntity<Map<String, Object>> request = new HttpEntity<>(requestBody, headers);
+
+            return restTemplate.postForEntity(endPoint, request, String.class);
+        } catch (HttpStatusCodeException hse) {
+            throw new HttpRequestException("HTTP request failed", hse.getStatusCode());
+        }
+        catch (Exception e) {
+            throw new Exception("HTTP POST request failed");
         }
     }
 }
